39ee9d025a0df4c1f6cb61083820628e9f7c0100
Roman Zakrevskyi <romul3003@gmail.com>
2020-10-18 18:22:03 +0300

    Fix user menu closing

--- src/blocks/modules/user-details/user-details.js
+++ src/blocks/modules/user-details/user-details.js
@@ -1,6 +1,29 @@
 const userMenuBtn = document.getElementById('userMenuBtn')
 const userMenu = document.getElementById('userMenu')
 
-userMenuBtn.addEventListener('click', event => {
-  userMenu.classList.toggle('user-details__menu_opened')
-})
+let isOpen = false
+
+userMenuBtn.addEventListener('click', toggle)
+
+function toggle() {
+  if (isOpen) close()
+  else open();
+}
+
+function open() {
+  userMenu.classList.add('user-details__menu_opened')
+  document.addEventListener('click', onDocumentClick)
+  isOpen = true
+}
+
+function close() {
+  userMenu.classList.remove('user-details__menu_opened');
+  document.removeEventListener('click', onDocumentClick);
+  isOpen = false;
+}
+
+function onDocumentClick(event) {
+  if (!userMenu.contains(event.target) && !userMenuBtn.contains(event.target)) {
+    close()
+  }
+}
